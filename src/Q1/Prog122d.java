2885d4fd20fcf10a8d7e66d9c6f277c263956ad7
purdy.c <purdy.c@stu.janesville.k12.wi.us>
2024-10-08 11:25:36 -0500

    Update

--- src/Q1/Prog122d.java
+++ src/Q1/Prog122d.java
@@ -6,9 +6,14 @@ public class Prog122d {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double x = -13;
+        double y = Math.pow(x, 6) - 3 * Math.pow(x, 5) - 93 * Math.pow(x, 4) + 87 * Math.pow(x, 3) + 1596 * Math.pow(x, 2) - 1280 * x - 2800;
+        while (x < 16) {
+
+            x += 1;
+            System.out.println(x);
 
 
-
-
+        }
     }
 }
