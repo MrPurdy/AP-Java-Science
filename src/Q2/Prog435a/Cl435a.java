96525f062856d4d9dea093f096073a21b49e2b76
Mr.Purdy <purdy.c@stu.janesville.k12.wi.us>
2024-12-27 15:29:51 -0600

    update

--- src/Q2/Prog435a/Cl435a.java
+++ src/Q2/Prog435a/Cl435a.java
@@ -1,48 +1,48 @@
 package Q2.Prog435a;
 
 public class Cl435a {
-        private int myCar;
-        private int myToll;
-        private double myCost;
-        private double[] tollnum = new double[8];
-        private double[] carnum = new double[6];
-        private String[] cartype = new String[6];
+    private int myCarType;
+    private int myToll;
+    private double myCost;
+    private double[] tollnum = new double[8];
+    private double[] carnum = new double[6];
+    private String[] car = new String[6];
 
-        public Cl435a(int Car, int Toll) {
-                myCar = Car;
-                myToll = Toll;
-                myCost = 0.0;
-                tollnum[0] = 1.35;
-                tollnum[1] = 2.00;
-                tollnum[2] = 2.50;
-                tollnum[3] = 3.25;
-                tollnum[4] = 4.10;
-                tollnum[5] = 4.8;
-                tollnum[6] = 5.50;
-                tollnum[7] = 6.0;
-                carnum[0] = 1.00;
-                carnum[1] = 1.30;
-                carnum[2] = 1.60;
-                carnum[3] = 2.00;
-                carnum[4] = 2.40;
-                carnum[5] = 2.70;
-                cartype[0] = "Compact Car";
-                cartype[1] = "Small Car";
-                cartype[2] = "Mis Size Car";
-                cartype[3] = "Full Size Car";
-                cartype[4] = "Truck";
-                cartype[5] = "16 Wheeler";
 
-        }
 
-        public void calc() {
-                myCost = tollnum[myToll - 1] * carnum[myCar - 1];
-        }
+    public Cl435a(int cartype, int toll) {
+        myCarType = cartype;
+        myToll = toll;
+        myCost = 0;
+        tollnum[0] = 1.35;
+        tollnum[1] = 2;
+        tollnum[2] = 2.5;
+        tollnum[3] = 3.25;
+        tollnum[4] = 4.1;
+        tollnum[5] = 4.8;
+        tollnum[6] = 5.5;
+        tollnum[7] = 6;
+        carnum[0] = 1;
+        carnum[1] = 1.3;
+        carnum[2] = 1.6;
+        carnum[3] = 2;
+        carnum[4] = 2.4;
+        car[0] = "Compact Car";
+        car[1] = "Small Car";
+        car[2] = "Medium Size Car";
+        car[3] = "Full Size Car";
+        car[4] = "Truck";
+        car[5] = "16 Wheeler";
+    }
 
-        public String toString() {
-                return cartype[myCar - 1] + "$"
-                        + carnum[myCar - 1] + "$"
-                        + tollnum[myToll - 1] + "$"
-                        + myCost;
-        }
+    public void calc() {
+        myCost = tollnum[myToll - 1] * carnum[myCarType - 1];
+    }
+
+public String toString() {
+        return car[myCarType - 1]
+                + "\t\t$" + carnum[myCarType - 1]
+                + "\t\t$" + tollnum[myToll - 1]
+                + "\t\t$" + myCost;
+}
 }
--- src/Q2/Prog435a/Prog435a.java
+++ src/Q2/Prog435a/Prog435a.java
@@ -3,31 +3,28 @@ package Q2.Prog435a;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.*;
 
 public class Prog435a {
     public static void main(String[] args) {
         try {
-            Scanner input = new Scanner(new File("Langdat/prog435a.dat"));
+            Scanner file = new Scanner(new File("Langdat/prog435a.dat"));
             Cl435a[] list = new Cl435a[100];
-            int count = 0;
+            int counter = 0;
 
-            while (input.hasNext()) {
-                int toll = input.nextInt();
-                int car = input.nextInt();
+            while (file.hasNext()) {
+                int cartype = file.nextInt();
+                int toll = file.nextInt();
 
-
-                if(car != -7) {
-                 Cl435a coolbeans = new Cl435a(car, toll);
-                 list[count] = coolbeans;
-                 count++;
-             }
-
+                if (cartype != -1) {
+                    Cl435a c = new Cl435a(cartype, toll);
+                    list[counter] = c;
+                    counter++;
+                }
             }
-            for (int lcv = 0; lcv < count; lcv++) {
+            for (int lcv = 0; lcv < counter; lcv++) {
                 list[lcv].calc();
             }
-            for (int lcv = 0; lcv < count; lcv++) {
+            for (int lcv = 0; lcv < counter; lcv++) {
                 System.out.println(list[lcv]);
             }
 
@@ -36,3 +33,24 @@ public class Prog435a {
         }
     }
 }
+
+/*
+Compact Car		$1.0		$1.35		$1.35
+Small Car		$1.3		$2.5		$3.25
+Medium Size Car	$1.6		$4.1		$6.56
+Full Size Car	$2.0		$5.5		$11.0
+Truck		    $2.4		$2.0		$4.8
+16 Wheeler		$0.0		$3.25		$0.0
+Compact Car		$1.0		$4.8		$4.8
+Small Car		$1.3		$6.0		$7.800000000000001
+Medium Size Car	$1.6		$1.35		$2.16
+Full Size Car	$2.0		$2.5		$5.0
+Truck		    $2.4		$4.1		$9.839999999999998
+16 Wheeler		$0.0		$5.5		$0.0
+Compact Car		$1.0		$6.0		$6.0
+Small Car		$1.3		$1.35		$1.7550000000000001
+Medium Size Car	$1.6		$2.0		$3.2
+Full Size Car	$2.0		$2.5		$5.0
+Truck		    $2.4		$3.25		$7.8
+16 Wheeler		$0.0		$4.1		$0.0
+ */
